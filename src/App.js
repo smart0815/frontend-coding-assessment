ec51bc8c313bd097e66b0227a6977900d866d4da
smart0815 <jack11112011@outlook.com>
2022-09-29 14:51:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>FE Coding Assessment</h2>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "./Slider";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Slider />
+  </React.StrictMode>,
+  rootElement
+);
